2e42e5840a4695d2aff5b9eb00f9d913b5989f00
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-09-11 17:26:38 +0530

    feat: implements CRUD api for issues table

---
+++ routes/issues.js
@@ -0,0 +1,57 @@
+const {Router} = require('express');
+const projectConnection =require('../database/issues');
+
+const routes = new Router();
+
+routes.get('/', async (req,res,next) => {
+    try {
+        let results = await projectConnection.fetchAllIssues();
+        return res.json(results);
+    }
+    catch(err) {
+        console.error(err)
+        next(new Error(`Internal server error, can't get list of all Issues`))
+    }
+});
+
+routes.post('/insert', async (req,res,next) =>{
+    try {
+        await projectConnection.insertIntoIssues(req.body);
+        return res.json(req.body);
+    }
+    catch(err) {
+        console.error(err)
+        next(new Error(`Internal server error, can't insert data into issue table`))
+
+    }
+})
+
+routes.delete('/delete/:id', async (req,res,next) => {
+    console.log(req)
+    try {
+        let results = await projectConnection.deleteRowFromIssuesTable(`${req.params.id}`);
+        return res.json(results.affectedRows);
+    }
+    catch(err) {
+        console.error(err)
+        next(new Error(`Internal server error, can't delete data from issue table`))
+
+    }
+})
+
+routes.put('/update/:id', async (req,res,next) => {    
+    console.log('=====================================')
+    console.log(req.params)
+    try {
+        let results = await projectConnection.updateRowFromIssuesTable(`${req.params.id}`,req.body );
+        console.log(results,"results")
+        return res.json(results);
+    }
+    catch(err) {
+        console.error(err)
+        next(new Error(`Internal server error, can't update values in issue table`))
+
+    }
+})
+
+module.exports = routes;
