390858b6aa12433b883a0c582a68c591596ad661
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-09-15 16:37:11 +0530

    feat: Adding dummy data on initialiazation of project

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const projectData = require('../data/project.json')
+const { v4: uuidv4 } = require('uuid');
 
 const { host, user, password, database } = require('../config');
 const databaseQuery = require('./creatDatabaseQuery')
@@ -7,7 +9,6 @@ let pool = mysql.createPool({
   host: host,
   user: user,
   password: password,
-  database: database
 });
 
 
@@ -23,6 +24,35 @@ pool.on('connection', function (connection) {
 
 });
 
+
+const InsertData = `INSERT INTO projects(project_id,project_name,created_by,created_on,expected_completion_time,description) VALUES
+                    ('${uuidv4()}','${projectData[0].project_name}','${projectData[0].created_by}','${projectData[0].created_on}','${projectData[0].expected_completion_time}','${projectData[0].description}'),
+                    ('${uuidv4()}','${projectData[1].project_name}','${projectData[1].created_by}','${projectData[1].created_on}','${projectData[1].expected_completion_time}','${projectData[1].description}'),
+                    ('${uuidv4()}','${projectData[1].project_name}','${projectData[2].created_by}','${projectData[2].created_on}','${projectData[2].expected_completion_time}','${projectData[2].description}')
+                    `
+
+const loadProjectData = async () => {
+
+  try{
+    const data = await queryPromise('SELECT * FROM projects')  
+
+    if(data.length == 0 || data[0].num_of_rows == 1 ){      
+      await queryPromise(InsertData)
+      console.log('data is inserted into project table')
+    }
+
+    else {
+      console.log('Data is Already present in project Table')
+    }
+  }
+
+  catch(err){
+    throw err;
+  }
+  
+};
+
+
 const queryPromise = (query) => {
   return new Promise((resolve, reject) => {
     pool.query(query, (err, results) => {
@@ -35,18 +65,43 @@ const queryPromise = (query) => {
 };
 
 
-const createDatabaseAndTable = () => {
-  const promiseArray = [
-    queryPromise(databaseQuery.createDb),
-    queryPromise(databaseQuery.useDatabase),
-    queryPromise(databaseQuery.createProjectTable),
-    queryPromise(databaseQuery.createIssueTable),
-    queryPromise(databaseQuery.createCommentTable),
-  ]
+const  createDatabaseAndTable = async () => {
 
-  Promise.all(promiseArray)
-    .then(() => console.log(`databases and tables are created`))
-    .catch(err => console.error(err))
+  try{
+    await queryPromise(databaseQuery.createDb)
+    console.log('Database created')
+  }
+  catch(err){
+    console.error(err)
+    throw err;
+  }
+
+  try{
+    await queryPromise(databaseQuery.useDatabase)
+    console.log('Using database')
+  }
+  catch(err){
+    console.error(err)
+    throw err;
+  }
+
+  try{
+    await queryPromise(databaseQuery.createProjectTable)
+    await queryPromise(databaseQuery.createIssueTable)
+    await queryPromise(databaseQuery.createCommentTable)
+    console.log('all tables are created')
+  }
+  catch(err){
+    console.error(err)
+    throw err;
+  }
+
+  try{
+    await loadProjectData()
+  }
+  catch(err){
+    console.error(err)
+  }
 
 }
 
--- index.js
+++ index.js
@@ -8,7 +8,26 @@ const project = require('./routes/project')
 const issues = require('./routes/issues')
 const comments = require('./routes/comments')
 
+const projectData = require('./data/project.json')
+
 createDatabaseAndTable();
+// let array= []
+// for(let i = 0; i < projectData.length;i++){
+//   array.push(`${projectData[i].created_by}","${projectData[i].created_on}`)
+// }
+// console.log(array)
+// console.log(JSON.stringify(Object.values(projectData[0])))
+
+// for(let i =0; i < projectData.length; i++){
+//   let a = [];
+//   let b = ''
+//   for(let j = 0; j < Object.values(projectData[i]).length; j++){
+//     b += '"' + Object.values(projectData[i])[j]+ '"' + ","
+//   }
+//   a.push(b)
+//   b = ''
+//   console.log(a)
+// }
 
 app.use(cors())
 app.use(express.json())
